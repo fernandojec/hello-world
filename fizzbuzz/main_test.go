deb91d50251766faeb8f55fa7ffe59a8aacd09a1
fernandojec <fernando.riyo@jec.co.id>
2023-06-12 21:19:54 +0700

    Unit Test 2

--- fizzbuzz/main_test.go
+++ fizzbuzz/main_test.go
@@ -5,8 +5,23 @@ import "testing"
 func TestGenerateWord(t *testing.T) {
 
 	t.Run("Should Return Buzz", func(t *testing.T) {
-		if got := GenerateWord(5); got == "Buzz" {
+		if got := GenerateWord(5); got != "Buzz" {
 			t.Errorf("GenerateWord() = %v, want %v", got, "Buzz")
+		}
+	})
+	t.Run("Should Return Fizz", func(t *testing.T) {
+		if got := GenerateWord(3); got != "Fizz" {
+			t.Errorf("GenerateWord() = %v, want %v", got, "Fizz")
+		}
+	})
+	t.Run("Should Return FizzBuzz", func(t *testing.T) {
+		if got := GenerateWord(15); got != "FizzBuzz" {
+			t.Errorf("GenerateWord() = %v, want %v", got, "FizzBuzz")
+		}
+	})
+	t.Run("Should Return 4", func(t *testing.T) {
+		if got := GenerateWord(4); got != "4" {
+			t.Errorf("GenerateWord() = %v, want %v", got, "4")
 		}
 	})
 	// type args struct {
